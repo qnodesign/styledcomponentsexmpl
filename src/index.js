d1d4ca63c7c104bc3b393251037e16b9bb850a63
qno <hefelle@gmail.com>
2018-06-23 11:38:03 +0200

    material

--- src/index.js
+++ src/index.js
@@ -1,32 +1,54 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import styled from "styled-components";
+import { AppContainer, Title } from "./style";
 import classNames from "classnames";
+import styled from "styled-components";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import { withStyles } from "@material-ui/core/styles";
 
 import "./styles.css";
 
-const AppContainer = styled.div`
-  text-align: center;
-  background: yellow;
+const Img = styled.div`
+  width: 200px;
+  height: 200px;
+  background-image: url(${({ imageSrc }) => imageSrc});
 `;
 
-const Title = styled.h1`
-  color: red;
-  background: yellow;
-`;
+const styles = {
+  customColor: {
+    background: "green",
+    "&:hover": {
+      background: "blue"
+    }
+  }
+};
 
-function App() {
+function App({ classes }) {
   const stg = true;
 
   return (
-    <AppContainer>
-      <Title>Hello CodeSandbox</Title>
-      <h2 className={classNames("a", "b", { c: stg })}>
-        Start editing to see some magic happen!
-      </h2>
-    </AppContainer>
+    <Grid container>
+      <Grid item>
+        <AppContainer>
+          <Title>Hello CodeSandbox</Title>
+          <Img
+            className="test"
+            imageSrc="https://lbbfinal.files.wordpress.com/2015/01/logo.gif"
+          />
+          <h2 className={classNames("a", "b", { c: stg })}>
+            Start editing to see some magic happen!
+          </h2>
+          <Button className={classes.customColor} variant="raised">
+            test
+          </Button>
+        </AppContainer>
+      </Grid>
+    </Grid>
   );
 }
 
+const AppWithStyles = withStyles(styles)(App);
+
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<AppWithStyles />, rootElement);
